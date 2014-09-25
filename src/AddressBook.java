9481bb0efa2b15b7a06575564625cec4a664e290
mitch cail <mitch.cail@gmail.com>
2014-09-25 15:04:43 -0400

    first commit

---
+++ src/AddressBook.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class AddressBook {
+
+	private List<BuddyInfo> buddies;
+
+	public AddressBook()
+	{
+		buddies = new ArrayList<BuddyInfo>();
+	}
+	public void addBuddy(BuddyInfo buddy)
+	{
+		buddies.add(buddy);
+	}
+	public void removeBuddy(BuddyInfo buddy)
+	{
+		buddies.remove(buddy);
+	}
+	public static void main(String[] args) {
+		BuddyInfo buddy = new BuddyInfo();
+		AddressBook book = new AddressBook();
+		buddy.setAddress("ottawa");
+		buddy.setNumber("123-456-6784");
+		
+		book.addBuddy(buddy);
+		
+		System.out.println("hello " + buddy.getName() );
+
+	}
+}
---
+++ src/BuddyInfo.java
@@ -0,0 +1,36 @@
+
+public class BuddyInfo {
+
+	/**
+	 * @param args
+	 */
+	
+	private String name;
+	private String address;
+	private String number;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+}
